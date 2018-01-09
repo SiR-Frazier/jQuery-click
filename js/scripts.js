cb5e4edeb545ea91faef05aa1f71e285d78023ce
bryce frazier and mike albers <bryce@>
2018-01-09 11:23:00 -0800

    add option to change bg color, reamore option

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,22 @@ $(document).ready(function() {
     $("#toggle-2").slideUp();
     $("#toggle-3").slideUp();
   });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  $("#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+  $("#read-more").click(function() {
+    $("#more-text").removeClass("hidden");
+    $("#read-more").addClass("hidden");
+    $("#more-text").slideDown(slow);
+  });
 });
