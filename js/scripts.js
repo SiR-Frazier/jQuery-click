8bfb8396465723e7ed0f46c4fdd5be196efd9163
bryce frazier and mike albers <mike@>
2018-01-09 10:30:18 -0800

    add slide function and add/remove classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $("#walrus-showing").slideToggle();
-    $("#walrus-hidden").slideToggle();
+  $("#click-show").click(function() {
+    $("#toggle-1").addClass("hidden");
+    $("#toggle-p").removeClass("hidden");
+    $("#toggle-2").slideDown();
+    $("#toggle-3").slideDown();
+  });
+  $("#click-hide").click(function() {
+    $("#toggle-1").removeClass("hidden");
+    $("#toggle-p").addClass("hidden");
+    $("#toggle-2").slideUp();
+    $("#toggle-3").slideUp();
   });
 });
